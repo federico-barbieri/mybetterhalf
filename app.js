863a42f81fce68ef869f2ad15be3190dfdcc07af
Federico Barbieri <federicobarbieri54@gmail.com>
2021-11-30 10:05:29 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const guide = document.querySelector('.guide');
 
 const author = document.querySelector('.author');
 
-const dentist = document.querySelector('.dentist');
 
 
 roulette.addEventListener('change', () => {
@@ -24,42 +23,80 @@ let count = 0;
 
 if (roulette.value == 1){
   count += 1;
-  handywoman.style.visibility ="visible";
+  handywoman.style.display ="flex";
   handywoman.style.zIndex = count;
+  lightbulb.style.display = "none";
+  ramen.style.display = "none";
+  pizza.style.display = "none";
+  rollerblader.style.display = "none";
+  guide.style.display = "none";
+  author.style.display = "none";
 }
 if (roulette.value == 2){
   count += 1;
-  lightbulb.style.visibility = "visible";
+  lightbulb.style.display = "flex";
   lightbulb.style.zIndex = count;
+  handywoman.style.display = "none";
+  ramen.style.display = "none";
+  pizza.style.display = "none";
+  rollerblader.style.display = "none";
+  guide.style.display = "none";
+  author.style.display = "none";
 }
 if (roulette.value == 3){
   count += 1;
-  ramen.style.visibility = "visible";
+  ramen.style.display = "flex";
   ramen.style.zIndex = count;
+  lightbulb.style.display = "none";
+  handywoman.style.display = "none";
+  pizza.style.display = "none";
+  rollerblader.style.display = "none";
+  guide.style.display = "none";
+  author.style.display = "none";
 }
 if (roulette.value == 4){
   count += 1;
-  pizza.style.visibility = "visible";
+  pizza.style.display = "flex";
   pizza.style.zIndex = count;
+  lightbulb.style.display = "none";
+  ramen.style.display = "none";
+  handywoman.style.display = "none";
+  rollerblader.style.display = "none";
+  guide.style.display = "none";
+  author.style.display = "none";
 }
 if (roulette.value == 5){
   count += 1;
-  rollerblader.style.visibility = "visible";
+  rollerblader.style.display = "flex";
   rollerblader.style.zIndex = count;
+  lightbulb.style.display = "none";
+  ramen.style.display = "none";
+  pizza.style.display = "none";
+  handywoman.style.display = "none";
+  guide.style.display = "none";
+  author.style.display = "none";
 }
 if (roulette.value == 6){
   count += 1;
-  guide.style.visibility = "visible";
+  guide.style.display = "flex";
   guide.style.zIndex = count;
+  lightbulb.style.display = "none";
+  ramen.style.display = "none";
+  pizza.style.display = "none";
+  rollerblader.style.display = "none";
+  handywoman.style.display = "none";
+  author.style.display = "none";
 }
 if (roulette.value == 7){
   count += 1;
-  author.style.visibility = "visible";
+  author.style.display = "flex";
   author.style.zIndex = count;
+  lightbulb.style.display = "none";
+  ramen.style.display = "none";
+  pizza.style.display = "none";
+  rollerblader.style.display = "none";
+  guide.style.display = "none";
+  handywoman.style.display = "none";
 }
-if (roulette.value == 8){
-  count += 1;
-  dentist.style.visibility = "visible";
-  dentist.style.zIndex = count;
-}
+
 })
